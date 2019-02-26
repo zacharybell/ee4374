ec0670535304bcdb167bb621081e91d0853daa8c
Zachary Bell <zacharybell@live.com>
2019-02-25 22:05:52 -0700

    Change formating

--- assignment2/zbell_prog2/zbell_prog2.c
+++ assignment2/zbell_prog2/zbell_prog2.c
@@ -1,6 +1,6 @@
 //
 //	EE 4374 Assignment # 2 Shell (Test Function)
-//	Author: ???
+//	Author: Zachary J Bell
 //
 
 #include <fcntl.h>
@@ -8,42 +8,37 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include "student_argtok.h"
-#include "student_exec.h"
+#include "zbell_argtok.h"
+#include "zbell_exec.h"
 
 int main()
 {
     int i;
-	int inputSize;
-	char buff[128];		// Character buffer
-	char **args;
-
+    int inputSize;
+    char buff[128];		// Character buffer
+    char **args;
+ 
     write(1, "Welcome to the Student Shell (type 'x' to exit)\n", 48);
-	while(1)
-	{
-		// Request a line of input
-		write(1, "\nshell -> ", 10);
-		// Read the line
-		inputSize = read(0, buff, 128);
-		// Strip off carriage return
-		buff[inputSize-1] = '\0';
+    
+    while(1) {
+        // Request a line of input
+ 	write(1, "\nshell -> ", 10);
+ 	// Read the line
+ 	inputSize = read(0, buff, 128);
+ 	// Strip off carriage return
+ 	buff[inputSize-1] = '\0';
+ 
+ 	if(buff[0] == '\0') continue;
+ 
+ 	if((buff[0] == 'x') && (buff[1] == '\0')) exit(0);
+ 
+ 	// make the argument vector
+ 	args = argtok(buff);
+ 
+ 	// execute the command
+ 	executeCmd(args);
+    }
 
-		if(buff[0] == '\0')
-			continue;
-
-		if((buff[0] == 'x') && (buff[1] == '\0'))
-		{
-			exit(0);
-		}
-
-		// make the argument vector
-		args = argtok(buff);
-
-		// execute the command
-		executeCmd(args);
-
-	}
-
-	return 0;
+    return 0;
 
 }
