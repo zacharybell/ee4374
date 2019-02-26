02e13e682e41be63ba7fa9e38c10f23f7da61fa3
Zachary Bell <zacharybell@live.com>
2019-02-25 22:07:15 -0700

    Add function declarations

--- assignment2/zbell_prog2/zbell_exec.h
+++ assignment2/zbell_prog2/zbell_exec.h
@@ -1,6 +1,12 @@
 //
 //	EE 4374 Assignment # 2 Command Executer
-//	Author: ???
+//	Author: Zachary J Bell
 //
+
+#define SPECIAL "|><"
+
+int next_special(char **args);
+
+int execBackground(char **args);
 
 int executeCmd(char **args);
