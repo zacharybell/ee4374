0e71b652cd0b2784ffaaf2687e4393358719b304
Zachary Bell <zacharybell@live.com>
2019-03-08 15:26:10 -0700

    Change search_prime prototype to support sPRIME_THREAD type as arg

--- assignment3/zbell_prog3/zbell_prime.h
+++ assignment3/zbell_prog3/zbell_prime.h
@@ -10,7 +10,7 @@
 typedef struct				// Prime search thread data
 {
 	unsigned int num;		// Prime search thread number
-	unsigned int current;	// Number currently evaluating for primality
+	unsigned int current;	        // Number currently evaluating for primality
 	unsigned int low;		// Low end of range to test for primality
 	unsigned int high;		// High end of range to test for primality
 } sPRIME_THREAD;
@@ -21,6 +21,6 @@ extern int numThreads;										// Number of prime search threads
 
 /* Function prototypes */
 int test_prime(int n);
-void *prime_search(void *param);
+void *prime_search(sPRIME_THREAD*);
 void *mini_shell(void *param);
 
