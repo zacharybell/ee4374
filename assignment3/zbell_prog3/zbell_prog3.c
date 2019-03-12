43837dea336abaa45e74d49e40d8fae4eeb3b0ad
Zachary Bell <zacharybell@live.com>
2019-03-11 21:00:46 -0700

    Add multithread functionality to test program in main
    Program splits the range of primes to search for based on the number
    of threads allocated. The program then creates threads for each subset
    of numbers and outputs these to a file called "prime*.txt" where * is
    the thread number. The main thread then waits for all the worker
    threads to complete and then concatenates all of the thread output
    files together to create a file called "primes.txt".

--- assignment3/zbell_prog3/zbell_prog3.c
+++ assignment3/zbell_prog3/zbell_prog3.c
@@ -1,14 +1,15 @@
 // UT El Paso EE 4374 Assignment 3
 // Multi-threaded Prime Number Search
-// Author: ???
+// Author: Zachary Bell
 //
 #include <pthread.h>
 #include <stdio.h>
 #include <signal.h>
+#include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include "student_prime.h"
+#include "zbell_prime.h"
 
 #define BUFFER_SIZE	1000000
 
@@ -18,75 +19,54 @@ FILE *primeFile;
 FILE *primeThreadFile;
 
 
-int main(int argc, char *argv[])
-{
-  int i, bytesRead, bytesWritten;
-  pthread_t tid[MAX_THREADS]; 
-  pthread_t tidshell;
-  pthread_attr_t attr;
-  time_t before, after;
+int main(int argc, char *argv[]) {
+    int i, bytesRead, bytesWritten, rc;
+    pthread_t tid[MAX_THREADS]; 
+    pthread_t tidshell;
+    pthread_attr_t attr;
+    time_t before, after;
 
-  /* Record time at start */
-  before = time(NULL);
+    /* Record time at start */
+    before = time(NULL);
 
-  /* Setup threads to find prime numbers */
-  pthread_attr_init(&attr);
-  numThreads = 2;
+    /* Setup threads to find prime numbers */
+    pthread_attr_init(&attr);
+    numThreads = 2;
   
+    /* Initialize thread data (i.e. number ranges */
+    init_sprime(1, 5000000); // 5 million !!!
 
-  /* Setup a mini shell thread to provide interactivity with the user */
-  pthread_create(&tidshell,&attr,mini_shell,NULL);
-  
-#if 0
 
-  /* Create primes output file */
-  primeFile = fopen("primest","w");
-  fclose(primeFile);
+    /* Setup a mini shell thread to provide interactivity with the user */
+    pthread_create(&tidshell,&attr,mini_shell,NULL);
 
-  /* Wait for the prime search threads to complete and combine their data */
-  for(i = 0; i < numThreads; i++)
-  {
-  	/* Wait for the next thread to complete */
-  	pthread_join(tid[i],NULL);
-  	/* On thread completion, write its data to "primest" */
-    fileName[0] = '\0';
-    sprintf(fileName, "primes%d", i+1);					// Open the thread's data file
-    if((primeThreadFile = fopen(fileName,"r")) == NULL)
-    {
-    	printf("Failed to open file: %s\n", fileName);
+
+    /* Setup worker threads */
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    for (i=0; i<numThreads; i++) {
+        pthread_create(&tid[i], &attr, prime_search, (void*)&primeThreadData[i]);
+    } 
+    pthread_attr_destroy(&attr);
+
+
+    /* Wait for threads to complete */
+    for (i=0; i<numThreads; i++) {
+        rc = pthread_join(tid[i], NULL);
+        if (rc) exit(-1);
     }
-    else
-    {
-    	if((primeFile = fopen("primest","a")) == NULL)	// Open "primest"
-    	{
-    		printf("Failed to open file: primest\n");
-    	}
-    	else
-    	{
-    		while(feof(primeThreadFile) == 0)
-			{
-				/* Read from the thread's data file */
-				bytesRead = fread(buffer,1,BUFFER_SIZE,primeThreadFile);
-				/* Write to the combined file */
-				bytesWritten = fwrite(buffer,1,bytesRead,primeFile);
-    		}
-			fclose(primeFile);
-    	}
-		fclose(primeThreadFile);
-	}
-  }
+
+    /* Concat both the thread files (prime*.txt) into primes.txt file. */
+    // used for testing (normally, I wouldn't hardcode this)
+    FILE* fp1 = fopen("prime1.txt", "r");
+    FILE* fp2 = fopen("prime2.txt", "r");
+    concat_files(fp1, fp2, "primes.txt"); 
+
+    sleep(20);
   
-  /* Record execution time */
-  after = time(NULL);
-  printf("\nPrime search done after %ld seconds\n", after-before);
-#endif
+        
+    /* Lastly, kill the interaction thread */
+    pthread_kill(tidshell, SIGKILL);
 
-  sleep(20);
-  
-  /* Lastly, kill the interaction thread */
-  pthread_kill(tidshell, SIGKILL);
-
-  return 0;
-
+    return 0;
 }
 
